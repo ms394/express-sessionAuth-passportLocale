04c610a1521a81456bbbff0f1593591e546d1194
ms394 <mohsinsajan394@gmail.com>
2022-11-22 13:59:08 +0530

    Express Session Implemented

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Initializing session
+app.use(
+  session({
+    secret: process.env.EXPRESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30days
+  })
+);
 
 app.get("/", (req, res) => {
   const date = new Date().toLocaleDateString();
