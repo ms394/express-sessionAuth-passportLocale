170a9c439dcd8edcf7365af0410f89fc89bfbc1f
ms394 <mohsinsajan394@gmail.com>
2022-11-23 18:42:37 +0530

    DB Configurations and DB connection setup

---
+++ config/dbConfig.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+});
+
+module.exports = pool;
